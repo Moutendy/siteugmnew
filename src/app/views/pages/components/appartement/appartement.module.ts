dc0473c469e6c72158902d4085f928c6438cca8e
christopher moutendy <christophermoutendy@gmail.com>
2023-01-16 17:03:56 +0100

    modifier

---
+++ src/app/views/pages/components/appartement/appartement.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CommentaireComponent } from './commentaire/commentaire.component';
+
+
+
+@NgModule({
+  declarations: [
+    CommentaireComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AppartementModule { }
