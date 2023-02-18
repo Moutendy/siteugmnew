6e773c7dac35bcaa66dd9a79fa6fb88a9beb9728
christopher moutendy <christophermoutendy@gmail.com>
2023-02-18 23:56:17 +0100

    modifier

--- src/app/views/pages/components/profil/profil.component.ts
+++ src/app/views/pages/components/profil/profil.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-profil',
@@ -6,7 +7,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./profil.component.css']
 })
 export class ProfilComponent {
+  constructor(
+    private fb: FormBuilder
+  ) { }
+  myFiles!: File ;
+  myFilesCouverture: string[] = [];
+  @ViewChild('input_file')
+  InputFileVariable!: ElementRef;
   ngOnInit(): void {
-    console.log('gsgsgsgsg');
+
+        }
+
+        onChange(event: any) {
+          for (var i = 0; i < event.target.files.length; i++) {
+           console.log(event.target.files[i]);
+            this.myFiles=event.target.files[i];
+          }
+
+          if(event.target.files.length > 0){
+            this.InputFileVariable.nativeElement.value = "";
+          }
+        }
+
+
+        getFiles() {
+          this.InputFileVariable.nativeElement.click();
+        }
+        getFilesCouverture() {
+          this.InputFileVariable.nativeElement.click();
+        }
+
+
+        onChangeCouverture(event: any) {
+
+          for (var i = 0; i < event.target.files.length; i++) {
+            this.myFilesCouverture.push(event.target.files[i]);
+
+          }
+
+          if(event.target.files.length > 0){
+            this.InputFileVariable.nativeElement.value = "";
+          }
+        }
+
+
+        sendprofil()
+        {
+console.log(this.myFiles.name);
+
         }
 }
